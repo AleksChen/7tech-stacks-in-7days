5f47b19159bc1b2d62cec5e9f3f18891a9c36045
chenwenwu <chenwenwu@kanyun.com>
2023-12-21 11:25:32 +0800

    feat: 🎸 init

---
+++ iOS/Mall Demo/Mall_DemoApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct Mall_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS/Mall Demo/MeView.swift
@@ -0,0 +1,20 @@
+//
+//  MeView.swift
+//  Mall Demo
+//
+//  Created by wenwuchen on 2023/12/21.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text("Hello, MeView!")
+    }
+}
+
+struct MeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeView()
+    }
+}
