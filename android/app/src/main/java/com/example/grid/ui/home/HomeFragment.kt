0517cc1eee91283da817c4e236a6418315773dcc
chenwenwu <chenwenwu@kanyun.com>
2023-12-28 14:25:39 +0800

    feat: 🎸 商品架构

--- android/app/src/main/java/com/example/grid/ui/home/HomeFragment.kt
+++ android/app/src/main/java/com/example/grid/ui/home/HomeFragment.kt
@@ -17,6 +17,7 @@ import com.bumptech.glide.Glide
 import com.example.grid.R
 import com.example.grid.databinding.FragmentHomeBinding
 import com.example.grid.utils.loadJSONFromAsset
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
@@ -49,7 +50,12 @@ class HomeFragment : Fragment() {
 
         // 在 Fragment 的 onCreateView 生命周期中调用 supportActionBar?.hide() 进行隐藏，但会把所有页面的操作栏都隐藏
         (requireActivity() as AppCompatActivity).supportActionBar?.hide()
-        requireActivity().window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        requireActivity()
+                .window
+                .setFlags(
+                        WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                        WindowManager.LayoutParams.FLAG_FULLSCREEN
+                )
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         return binding.root
@@ -76,20 +82,40 @@ class HomeFragment : Fragment() {
             val salePrice: Double,
             val img: String,
             val introduction: String,
-            val tagImage: String
+            val tagImage: String,
+            var count: Double
     )
 
     data class ProductList(val list: List<Product>)
+
+    fun updateCartDisplay(totalPrice: Double) {
+        val cartView = binding.root.findViewById<ExtendedFloatingActionButton>(R.id.totalPrice)
+
+        if (totalPrice > 0) {
+            // 如果 totalPrice 大于 0，显示购物车并更新总价
+            cartView.visibility = View.VISIBLE
+            cartView.text = "总价：¥${totalPrice.toString()}"
+        } else {
+            // cart. = View.GONE
+            cartView.visibility = View.GONE
+        }
+    }
 
     inner class ProductAdapter(private val products: List<Product>) :
             RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
 
         inner class ProductViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
             val productImage: ImageView = view.findViewById(R.id.productImage)
+            val productFirstRow = view.findViewById<View>(R.id.productFirstRow)
+
             val tagImage: ImageView = view.findViewById(R.id.tagImage)
             val nameTextView: TextView = view.findViewById(R.id.productName)
             val introductionTextView: TextView = view.findViewById(R.id.productIntroduction)
             val priceTextView: TextView = view.findViewById(R.id.productPrice)
+
+            val minusButton: ImageView = view.findViewById(R.id.minus)
+            val addButton: ImageView = view.findViewById(R.id.add)
+            val countTextView: TextView = view.findViewById(R.id.count)
 
             fun bind(product: Product) {
                 nameTextView.text = product.name
@@ -109,22 +135,57 @@ class HomeFragment : Fragment() {
             }
 
             init {
-                view.setOnClickListener {
-                    // val product = products[adapterPosition]
+                productImage.setOnClickListener {
+                    findNavController().navigate(R.id.action_navigation_home_to_detailFragment)
+                }
+
+                productFirstRow.setOnClickListener {
                     findNavController().navigate(R.id.action_navigation_home_to_detailFragment)
                 }
             }
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
-            val layoutInflater = LayoutInflater.from(parent.context)
-            val view = layoutInflater.inflate(R.layout.item_product, parent, false)
+            val view =
+                    LayoutInflater.from(parent.context)
+                            .inflate(R.layout.item_product, parent, false)
 
             return ProductViewHolder(view)
         }
 
         override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
-            holder.bind(products[position])
+            val product = products[position]
+            holder.bind(product)
+
+            var totalPrice: Double
+
+            if (product.count < 1) {
+                holder.minusButton.visibility = View.GONE
+                holder.countTextView.visibility = View.GONE
+            }
+
+            holder.minusButton.setOnClickListener {
+                product.count--
+                if (product.count > 0) {
+                    holder.countTextView.text = product.count.toInt().toString()
+                } else {
+                    holder.countTextView.visibility = View.GONE
+                    holder.minusButton.visibility = View.GONE
+                }
+                totalPrice = products.map { it.salePrice * it.count }.sum()
+                updateCartDisplay(totalPrice)
+            }
+
+            holder.addButton.setOnClickListener {
+                product.count++
+                if (product.count >= 1) {
+                    holder.countTextView.text = product.count.toInt().toString()
+                    holder.countTextView.visibility = View.VISIBLE
+                    holder.minusButton.visibility = View.VISIBLE
+                }
+                totalPrice = products.map { it.salePrice * it.count }.sum()
+                updateCartDisplay(totalPrice)
+            }
         }
 
         override fun getItemCount() = products.size
